b5be7f4cf695e1c96902d4e4aac8b1f20a3a7b9f
Linus Unnebäck <linus@folkdatorn.se>
2017-10-31 14:50:23 +0000

    🎉 Make sha 256 hashing work in browser

---
+++ browser/sha256.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const arrayBufferToHex = require('array-buffer-to-hex')
+const encodeUtf8 = require('encode-utf8')
+const hexToArrayBuffer = require('hex-to-array-buffer')
+const rawSha256 = require('crypto-digest-sync/sha256')
+
+const SRPInteger = require('../lib/srp-integer')
+
+function concat (buffers) {
+  const length = buffers.reduce((mem, item) => mem + item.byteLength, 0)
+  const combined = new Uint8Array(length)
+
+  buffers.reduce((offset, item) => {
+    combined.set(new Uint8Array(item), offset)
+    return offset + item.byteLength
+  }, 0)
+
+  return combined.buffer
+}
+
+module.exports = function sha256 (...args) {
+  const buffer = concat(args.map((arg) => {
+    if (arg instanceof SRPInteger) {
+      return hexToArrayBuffer(arg.toHex())
+    } else if (typeof arg === 'string') {
+      return encodeUtf8(arg)
+    } else {
+      throw new TypeError('Expected string or SRPInteger')
+    }
+  }))
+
+  return SRPInteger.fromHex(arrayBufferToHex(rawSha256(buffer)))
+}
